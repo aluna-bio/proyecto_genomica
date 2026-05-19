b36a3b1ecd14647b2474144fe361800268b57f51
Paola Martínez Gámez <pmartinez66@alumnos.uaq.mx>
2026-05-19 14:53:22 -0600

    Agregue los paquetes a utilizar, cree la matriz de transposición a partir del objeto RDS, y use la función "pickSoftThreshold" para buscar el mejor power, faltan  las graficaspara visualizar.

--- 02_Scripts/Script1.R
+++ 02_Scripts/Script1.R
@@ -1,3 +1,9 @@
+#Instalar paquetes a utilizar
+BiocManager::install("WGCNA", force = TRUE)
+library(WGCNA)
+install.packages("igraph")
+library(igraph)
+
 #Investigamos y esta función ignora los demás metadatos
 #datos <- read.delim("01_RawData/GSE25066_series_matrix.txt",
                     comment.char = "!",
@@ -20,3 +26,10 @@
 #saveRDS(datos_filtrados, file = "01_RawData/datos.rds") #esto ya no lo tienen que hacer porque ya lo guardé en la carpeta 
 datos_filtrados<- readRDS( file = "01_RawData/datos.rds" )
 
+#transposición, para cambiar los genes de fila a columna, y las muestras de columna a fila
+datos_t <- t(datos_filtrados)
+
+# POWER: para determinar el numero por el cual se hara la potencia
+# Esto fortalece la correlación fuerte y debilita la correlación débil o negativa, haciendo que el valor de correlación sea más consistente conRed sin escalaLas características son biológicamente más significativas. 
+#Esta función busca el mejor power:
+pst <- pickSoftThreshold(datos_t, powerVector = (1:20), networkType = "unsigned") 
